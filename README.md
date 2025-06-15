01ad756ccc7cd1b537a8fc33698278d14b379f99
arymus <arymusr@gmail.com>
2025-06-15 12:13:09 -0400

    Wrote all the code in one go... Oops?

---
+++ README.md
@@ -0,0 +1,6 @@
+# Program Runner
+A CLI tool I made for my new laptop which uses Arch Linux
+Warning... I'm fairly sure this won't work outside of my laptop
+
+I made this because I thought it would be cool to open Roblox (using [Sober](https://sober.vinegarhq.org/)) from the terminal.
+It also has the ability to open [Konsole](https://konsole.kde.org/), a KDE terminal emulator as well as [Kitty](https://github.com/kovidgoyal/kitty), a GPU-based terminal emulator
---
+++ bin/script.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+/* ^ Allows us to run this file without the node command
+ * by passing the node interpreter as the first argument in the terminal
+ * so that the second argument (the one we input) is the file name
+ * (in this case an alias for the file, which would be notebook)
+ * 
+ * https://en.wikipedia.org/wiki/Shebang_(Unix)
+ */
+
+const arg = require("arg"); // Import the arg package for parsing arguments
+
+// Try to run this block of code
+try {
+
+    // Define arguments
+    const args = arg({
+    "--help": Boolean, // --help flag for showing commands, which can be true or false as an argument
+	"--version": Boolean, // --version flag for showing the version, which can be true or false as an argument
+    "-h": "--help", // -h shorthand for --help
+	"-v": "--version", // -v shorthand for --version
+    });
+
+    
+    // If the argument passed with the command is "--help" or its shorthand "-h"
+    if (args["--help"]) {
+
+        // Log the instructions
+	    console.log("open roblox: Opens Roblox\nopen konsole: Opens Konsole\n--help/-h: Show this message");
+
+    // If the argument passed with the command is "--version" or its shorthand "-v"
+    } else if (args["--version"]) {
+        const { version } = require("../package.json"); // Import the version from package.json
+        console.log(version); // Log the version
+
+    // If else
+    } else {
+	    const run = require("../lib/commands.js"); // Import the run() function from commands.js
+        run(process.argv[2]); // Pass the second argument to the run function as the command
+    };
+
+// Catch the errors, represented as err
+} catch (err) {
+    console.log(err); // Log the error
+};
